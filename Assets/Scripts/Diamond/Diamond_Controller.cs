26eacf0a9ee7d99dbad97a2b1d12d38ceae15b7b
Kadeem Cherman (Kassie) <86675041+KassieSanisclaws@users.noreply.github.com>
2024-02-08 07:16:44 -0500

    first push

--- Assets/Scripts/Diamond/Diamond_Controller.cs
+++ Assets/Scripts/Diamond/Diamond_Controller.cs
@@ -11,7 +11,7 @@ public class Diamond_Controller : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-        {
+        { 
             anim.SetTrigger("Diamond_Destroyed");
 
             Destroy(gameObject);
